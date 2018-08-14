6ba6046ad665775ef4836b7ed1f1bb432584c51b
Lindsay Lindner <lindnerdesign@gmail.com>
2018-08-14 13:08:28 -0500

    progress 8.14

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,97 +12,51 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/", function(req, res) {
-    // Add sequelize code to find all posts, and return them to the user with res.json
-    db.burgers.findAll({
+// FindAll
+app.get("/", function(req, res) {
+ db.burgers.findAll({})
 
-    }).then(function(cb) {
-      // We have access to the todos as an argument inside of the callback function
+ .then(function(cb) { 
+  res.json(cb);
+ });
+});
+
+// Create
+app.post("/api/burgers", function(req, res) {
+  console.log(req.body);
+
+  db.burgers.create ({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured
+
+  }).then(function(cb) {
+    res.json(cb);
+  });
+});
+
+// Update
+app.put("/api/burgers/:id", function(req, res) {
+  console.log(req.body);
+  db.burgers.update(req.body,
+    {
+    where: {
+      id: req.body.id
+    }
+  })
+    .then(function(cb) {
       res.json(cb);
     });
-  });
+});
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    // Add sequelize code to find all posts where the category is equal to req.params.category,
-    // return the result to the user with res.json
-    db.Post.findAll({
-      category: req.params.category
-
-    }).then(function(cb) {
-      // We have access to the todos as an argument inside of the callback function
+// Destroy
+app.delete("/api/burgers/:id", function(req, res) {
+  db.burgers.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function(cb) {
       res.json(cb);
     });
-  });
-
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    // Add sequelize code to find a single post where the id is equal to req.params.id,
-    // return the result to the user with res.json
-    db.Post.findOne({
-      where: {
-      id: req.params.id,
-      }
-    }).then(function(cb) {
-      // We have access to the todos as an argument inside of the callback function
-      res.json(cb);
-    });
-  });
-
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    // Add sequelize code for creating a post using req.body,
-    // then return the result using res.json
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-
-    }).then(function(cb) {
-      // We have access to the new todo as an argument inside of the callback function
-      res.json(cb);
-    })
-      .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
-  });
-
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    // Add sequelize code to delete a post where the id is equal to req.params.id, 
-    // then return the result to the user using res.json
-    db.Post.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(cb) {
-      res.json(cb);
-    });
-
-  });
-
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    // Add code here to update a post using the values in req.body, where the id is equal to
-    // req.body.id and return the result to the user using res.json
-    db.Post.update({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(cb) {
-      res.json(cb);
-    })
-      .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
-  });
+});
 };
--- server.js
+++ server.js
@@ -5,27 +5,28 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var db = require("./models");
 
 // parse application/x-www-form-urlencoded
+// =============================================================
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars
-var exphbs = require("express-handlebars");
 
+app.use(express.static("public"));
+
+// Set Handlebars
+// =============================================================
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them
-var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
 // Start our server so that it can begin listening to client requests
+// =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
